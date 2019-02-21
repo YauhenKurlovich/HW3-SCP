656bfe0e2e298d3eda2c5890bcb5f0f17180041d
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-02-21 16:36:28 +0300

    UI: FINAL task is done (STARTING REFACTORING)

--- Final Task/web/teams/webapp/controller/App.controller.js
+++ Final Task/web/teams/webapp/controller/App.controller.js
@@ -6,8 +6,9 @@ sap.ui.define([
 	'sap/m/StandardListItem',
 	'sap/ui/core/mvc/Controller',
 	'sap/ui/model/json/JSONModel',
-	'./AjaxTeam'
-], function (jQuery, Button, Dialog, List, StandardListItem, Controller, JSONModel, AjaxTeam) {
+	'./AjaxTeam',
+	'./AjaxPlayer'
+], function (jQuery, Button, Dialog, List, StandardListItem, Controller, JSONModel, AjaxTeam,AjaxPlayer) {
 	"use strict";
 
 	this.editModeOn = function (items, index) {
@@ -20,8 +21,19 @@ sap.ui.define([
 		items[index].getCells()[6].setEnabled(false);
 	}
 
+	this.editModePOn = function (items, index) {
+		items[index].getCells()[7].setEnabled(true);
+	}
+
+	this.editModePOff = function (items, index) {
+		items[index].getCells()[2].setEditable(false);
+		items[index].getCells()[3].setEditable(false);
+		items[index].getCells()[7].setEnabled(false);
+	}
+
 	return Controller.extend("teams.controller.App", {
 		fixedSizeDialog: null,
+		fixedSizeDialogP: null,
 
 		onEdit: function (oEvent) {
 			var oTable = this.getView().byId('idTeamsTable');
@@ -49,6 +61,46 @@ sap.ui.define([
 			}
 		},
 
+		onEditP: function (oEvent) {
+			var oTable = this.getView().byId('idPlayersTable');
+			var aItems = oTable.getItems();
+
+			for(var i = 0; i < aItems.length; i ++)
+			{
+				for (var j = 2; j < 4; j++) {
+					aItems[i].getCells()[j].setEditable();
+				}
+				editModePOn(aItems, i);
+			}
+		},
+
+		offEditP: function (oEvent) {
+			var oTable = this.getView().byId('idPlayersTable');
+			var aItems = oTable.getItems();
+
+			for(var i = 0; i < aItems.length; i ++)
+			{
+				for (var j = 2; j < 4; j++) {
+					aItems[i].getCells()[j].setEditable(false);
+				}
+				editModePOff(aItems, i);
+			}
+		},
+
+		onItemPress : function (oEvent){
+			// var oTable = this.getView().byId('idTeamsTable');
+			// var selItem = oTable.getSelectedItem();
+			// var aItems = oTable.getItems();
+			// var index = oTable.indexOfItem(selItem); 
+			// var id = aItems[index].getCells()[0].getValue();
+			// var oPlayersTable = this.getView().byId('idPlayersTable');
+			
+			// oPlayersTable.bindItems({
+			// 	path: "/Players",
+			// 	model: "dataModel"
+			//   });
+		},
+
 		onSave: function (oEvent) {
 			var oTable = this.getView().byId('idTeamsTable');
 			var selItem = oTable.getSelectedItem();
@@ -69,6 +121,29 @@ sap.ui.define([
 			var id = selItem.getBindingContext("dataModel").getObject().teamId;
 
 			$.ajax(AjaxTeam.deleteTeam(id)).done(function (response) {
+				selItem.getBindingContext("dataModel").getModel().refresh(true);
+			});
+		},
+
+		onSaveP: function (oEvent) {
+			var oTable = this.getView().byId('idPlayersTable');
+			var selItem = oTable.getSelectedItem();
+			var aItems = oTable.getItems();
+			var index = oTable.indexOfItem(selItem);
+			var id = aItems[index].getCells()[0].getValue();
+			var name = aItems[index].getCells()[2].getValue();
+			var country = aItems[index].getCells()[3].getValue();
+			$.ajax(AjaxPlayer.updatePlayer(id, name, country)).done(function (response) {
+				selItem.getBindingContext("dataModel").getModel().refresh(true);
+			});
+			editModeOff(aItems, index);
+		},
+		onDeleteP: function (oEvent) {
+			var oTable = this.getView().byId('idPlayersTable');
+			var selItem = oTable.getSelectedItem();
+			var id = selItem.getBindingContext("dataModel").getObject().pId;
+
+			$.ajax(AjaxPlayer.deletePlayer(id)).done(function (response) {
 				selItem.getBindingContext("dataModel").getModel().refresh(true);
 			});
 		},
@@ -122,6 +197,66 @@ sap.ui.define([
 			}
 
 			this.fixedSizeDialog.open();
+		},
+
+		onAddP: function (oEvent) {
+			if (!this.fixedSizeDialogP) {
+				this.fixedSizeDialogP = new Dialog({
+						title: 'Add new Player',
+						contentWidth: "370px",
+						contentHeight: "200px",
+						content: [
+							new sap.m.Label({
+								text: "Player Name"
+							}),
+							new sap.m.Input({
+								maxLength: 20,
+								id: "PName"
+							}),
+
+							new sap.m.Label({
+								text: "Country"
+							}),
+							new sap.m.Input({
+								maxLength: 20,
+								id: "Country"
+							}),
+
+							new sap.m.Label({
+								text: "Id Team"
+							}),
+							new sap.m.Input({
+								maxLength: 20,
+								id: "tId"
+							})
+
+						],
+
+						beginButton: new Button({
+							text: 'Add',
+							press: function () {
+								var oTable = this.getView().byId('idPlayersTable');
+								var playerName = sap.ui.getCore().byId("PName").getValue();
+								var country = sap.ui.getCore().byId("Country").getValue();
+								var tId = sap.ui.getCore().byId("tId").getValue();
+
+								$.ajax(AjaxPlayer.createPlayer(playerName, country,tId)).done(function (response) {
+									oTable.getModel("dataModel").refresh(true);
+								});
+								this.fixedSizeDialogP.close();
+							}.bind(this)
+						}),
+						endButton: new Button({
+							text: 'Close',
+							press: function () {
+								this.fixedSizeDialogP.close();
+							}.bind(this)
+						})
+					}),
+					this.getView().addDependent(this.fixedSizeDialogP);
+			}
+
+			this.fixedSizeDialogP.open();
 		}
 
 	});
