5a52691228b5fa06a49897afaa891acf1e335fd6
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-02-22 16:45:09 +0300

    scope check is done

--- HW6/service/src/main/java/com/leverx/leverxspringdemo/controller/HomeController.java
+++ HW6/service/src/main/java/com/leverx/leverxspringdemo/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.leverx.leverxspringdemo.controller;
 
 import com.leverx.leverxspringdemo.domain.Destination;
+import com.leverx.leverxspringdemo.service.SecurityService;
 import com.sap.cloud.sdk.cloudplatform.CloudPlatform;
+import com.sap.cloud.sdk.cloudplatform.security.RolesAllowed;
+import com.sap.cloud.sdk.s4hana.connectivity.exception.AccessDeniedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +24,10 @@ public class HomeController {
 	@Autowired
 	private CloudPlatform platform;
 
+
+	@Autowired
+	private SecurityService securityService;
+
 	@RequestMapping(value="/destinations", method=RequestMethod.GET)
 	public String getHomes(Model model) {
 		String appName = platform.getApplicationName();
@@ -35,5 +42,34 @@ public class HomeController {
 		String appName = cloudService.getApplicationName();
 		model.addAttribute("appName", appName);
 		return "index";
+	}
+
+	@RequestMapping(value="/failed", method=RequestMethod.GET)
+	public String getRoleCheckFailed() {
+		return "scope";
+	}
+
+	@RequestMapping(value="/scope", method=RequestMethod.GET)
+	public String checkScope() throws AccessDeniedException {
+		securityService.userHasAuthorization("Display");
+		return "scope";
+	}
+
+	@RequestMapping(value="/scopeFail", method=RequestMethod.GET)
+	public String checkScopeFailed() throws AccessDeniedException {
+		securityService.userHasAuthorization("Download");
+		return "scope";
+	}
+
+	@RolesAllowed("ADMIN")
+	@RequestMapping(value="/admin", method=RequestMethod.GET)
+	public String checkAdmin() {
+		return "scope";
+	}
+
+	@RolesAllowed("HACKER")
+	@RequestMapping(value="/hacker", method=RequestMethod.GET)
+	public String checkHacker() {
+		return "scope";
 	}
 }
