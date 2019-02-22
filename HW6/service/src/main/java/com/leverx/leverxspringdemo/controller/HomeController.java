c02da5bee7a801c672303355c84bf4423057039f
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-02-22 16:04:13 +0300

    getDestinations & get app Name is done

--- HW6/service/src/main/java/com/leverx/leverxspringdemo/controller/HomeController.java
+++ HW6/service/src/main/java/com/leverx/leverxspringdemo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.leverx.leverxspringdemo.controller;
 
+import com.leverx.leverxspringdemo.domain.Destination;
+import com.sap.cloud.sdk.cloudplatform.CloudPlatform;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,13 +10,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.leverx.leverxspringdemo.service.CloudService;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 	
 	@Autowired
 	private CloudService cloudService;
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+
+	@Autowired
+	private CloudPlatform platform;
+
+	@RequestMapping(value="/destinations", method=RequestMethod.GET)
+	public String getHomes(Model model) {
+		String appName = platform.getApplicationName();
+		model.addAttribute("appName", appName);
+		List<Destination> destinations = cloudService.getDestinations();
+		model.addAttribute("destinations", destinations);
+		return "index";
+	}
+
+	@RequestMapping(value="/appname", method=RequestMethod.GET)
 	public String getHome(Model model) {
 		String appName = cloudService.getApplicationName();
 		model.addAttribute("appName", appName);
